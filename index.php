215139a2e8540e2c23e9e6e4e3b0a36955c6f8c8
AirMaks <rm1010@yandex.ru>
2020-11-26 18:35:37 +0300

    Rewrite function

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
-$str1 = "Аргентина манит негра";
+
+$str1 = "Кири лл ирик";
 $str2 = "Sum sumamus mus";
 $str3 = "acdscaca";
-check($str3);
-function check($str){
-    $string = mb_strtolower(str_replace(' ', '', $str));
-    $stri1 = "";
-    $stri = "";
-    $stri2 = "";
-    $stri3 = "";
-    for($i = 0, $j = strlen($string) - 1; $i < $j; $i++, $j--){
-        if(!(is_int(strlen($string) / 2))){
-            $stri3 = $string[$i+1];
-        }
-        if($string[$i] == $string[$j] ){
-            $stri1 .= $string[$i];
-            $stri .= $string[$j];
-            $stri2 = strrev($stri);
-        }
-    }
-    if ($stri1 != $stri2){ echo $stri1 ."". $stri3 ."". $stri2; }else{ echo $string[0]; }
+
+
+echo check($str1)."<br>";
+echo check($str2)."<br>";
+echo check($str3);
+
+
+function check($str) {
+
+        $str = preg_split('//u', $str);
+        $str = array_reverse($str);
+        $str = join($str);
+        return $str;
 }
+
 ?>
